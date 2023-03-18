afe4e6e1f4563e504275938d5c55f6d62d54f3f1
andrei-crisan <andreicrisan@protonmail.com>
2023-03-18 15:45:39 +0200

    DrivingSchool : Service_implemented

--- src/app/service/school.service.ts
+++ src/app/service/school.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { School } from '../model/school.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchoolService {
+  private schoolRestUrl = 'http://localhost:8080/schools';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllDrivingSchools(): Observable<School[]> {
+    return this.httpClient.get<Array<School>>(this.schoolRestUrl);
+  }
+
+  getDrivingSchoolById(reviewId: number): Observable<School> {
+    const schoolRestUrlFindOne = `${this.schoolRestUrl}/${reviewId}`;
+    return this.httpClient.get<School>(schoolRestUrlFindOne);
+  }
+
+  saveDrivingSchool(school: School): Observable<School> {
+    return this.httpClient
+      .post<School>(this.schoolRestUrl, school);
+  }
+
+  updateDrivingSchool(review: School): Observable<School> {
+    return this.httpClient
+      .put<School>(this.schoolRestUrl, review);
+  }
+
+  deleteDrivingSchoolById(reviewId: number): Observable<School> {
+    const schoolRestUrlDeleteOne = `${this.schoolRestUrl}/${reviewId}`;
+    return this.httpClient.delete<School>(schoolRestUrlDeleteOne);
+  }  
 }
+
