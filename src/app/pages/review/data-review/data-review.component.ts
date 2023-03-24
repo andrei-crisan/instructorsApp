c586ac206c0b28eb361d86a162aae81ee38c3b66
andrei-crisan <andreicrisan@protonmail.com>
2023-03-24 10:43:39 +0200

    fixed, review-update, modal

--- src/app/pages/review/data-review/data-review.component.ts
+++ src/app/pages/review/data-review/data-review.component.ts
@@ -12,29 +12,33 @@ import { ReviewService } from 'src/app/service/review.service';
 export class DataReviewComponent implements OnInit {
 
   constructor(
-    private reviewService: ReviewService, 
+    private reviewService: ReviewService,
     private matDialog: MatDialogRef<DataReviewComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
-    selectedReview : Review = this.data;
+  selectedReview: Review = this.data;
+  decodifedRating : string = this.selectedReview.experienceRating == 2 ? 'positive' : 'negative';
+  ratingSentiment: string[] = ['positive', 'negative'];
 
   ngOnInit(): void {
   }
 
-  updateReview(instructorReview: string, experienceRating: number) {
+  updateReview(instructorReview: string, experienceRating: string) {
 
     let instructor: Instructor = {
-      id: this.selectedReview.instructor?.id, 
+      id: this.selectedReview.instructor?.id,
       instructorName: this.selectedReview.instructor?.instructorName,
       instructorSurname: this.selectedReview.instructor?.instructorSurname,
       drivingSchoolName: this.selectedReview.instructor?.drivingSchoolName,
       drivingSchoolAddress: this.selectedReview.instructor?.drivingSchoolAddress
     }
 
-    let reviewToSave: Review = { 
-      id: this.selectedReview.id, 
-      instructorReview, 
-      experienceRating,
+    let experienceRatingToSend: number = experienceRating == 'positive' ? 2 : 1;
+
+    let reviewToSave: Review = {
+      id: this.selectedReview.id,
+      instructorReview,
+      experienceRating: experienceRatingToSend,
       instructor: instructor
     }
 
@@ -49,5 +53,7 @@ export class DataReviewComponent implements OnInit {
   closeModalComponent() {
     this.matDialog.close();
   }
+
+  
 
 }
