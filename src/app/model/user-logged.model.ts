935474fbf01cb84f35545ed4a6924e40a57d2137
andrei-crisan <andreicrisan@protonmail.com>
2023-09-08 12:27:52 +0300

    fix_operations

--- src/app/model/user-logged.model.ts
+++ src/app/model/user-logged.model.ts
@@ -1,4 +1,4 @@
 export interface UserLogged{
-    id: number,
-    name: string
+    sub: string,
+    roles: string[]
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -5,6 +5,7 @@ import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import jwt_decode from "jwt-decode";
 import { HttpErrorResponse } from '@angular/common/http';
+import { UserLogged } from '../model/user-logged.model';
 
 
 @Injectable({
@@ -16,7 +17,7 @@ export class AuthService {
 
   private _hasPermission$ = new BehaviorSubject<boolean>(false);
   hasPermission$ = this._hasPermission$.asObservable();
-
+  loggedUser!: UserLogged;
 
   get token() {
     return localStorage.getItem('instructors auth');
@@ -42,6 +43,7 @@ export class AuthService {
       .subscribe({
         next: result => {
           localStorage.setItem('instructors auth', result.token);
+          this.loggedUser = this.getLoggedUser(JSON.stringify(result.token));
           this._isLoggedIn$.next(true);
           this.route.navigate(['/reviews']);
         },
@@ -64,6 +66,10 @@ export class AuthService {
           alert(JSON.stringify(err.error.message));
         },
       });
+  }
+
+  private getLoggedUser(token: string): UserLogged {
+    return JSON.parse(atob(token.split('.')[1])) as UserLogged;
   }
 
 }
